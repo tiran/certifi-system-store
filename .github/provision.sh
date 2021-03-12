e0a02631def845dc2e6e24c071eb76a5d3c48895
Christian Heimes <christian@python.org>
2021-03-12 17:39:29 +0100

    test on opensuse/leap

--- .github/provision.sh
+++ .github/provision.sh
@@ -19,6 +19,10 @@ elif [ -e /usr/bin/apt ]; then
 elif [ -e /sbin/apk ]; then
     apk add python3 py3-pip
     apk add python3-dev gcc musl-dev
+elif [ -e /usr/bin/zypper ]; then
+    zypper update -y
+    zypper install -y python3-pip
+    zypper clean
 else
     echo "Distro not supported"
     exit 1
