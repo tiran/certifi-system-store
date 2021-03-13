4e15c3af93ff39a571bb3ee7395cdfbb0ec24ad5
Christian Heimes <christian@python.org>
2021-03-13 13:28:17 +0100

    Verify that setuptools can handle our setup.cfg

--- .github/entry.sh
+++ .github/entry.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -ex
 
-cd /tmp
-python3 -m tox \
-    --skip-missing-interpreters \
-    -c /workdir/tox.ini \
-    --workdir /tmp/tox
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+fi
+
+# check that default setuptools can handle setup.cfg and setup.py
+python3 -m pip list
+if [ "$VERSION_ID" != "18.04" ]; then
+    # setuptools on Ubuntu Bionic doesn't like attr: with src/
+    python3 setup.py egg_info sdist
+fi
+
+# update to latest setuptools, pipm and tox
+python3 -m pip install --upgrade pip setuptools tox
+python3 -m tox --skip-missing-interpreters
