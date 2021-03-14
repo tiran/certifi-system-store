ab2f0472dd1a8a2346e0e423fe3de2c19ccd1e40
Christian Heimes <christian@python.org>
2021-03-14 12:09:02 +0100

    Test SSLContext.load_verify_locations()
    Signed-off-by: Christian Heimes <christian@python.org>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 import certifi
 import pkg_resources
@@ -21,3 +22,11 @@ def test_version():
     for distname in ("certifi", "certifi_system_store"):
         dist = pkg_resources.get_distribution(distname)
         assert dist.version.startswith("30")
+
+
+def test_ssl_load_certs():
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    assert not ctx.get_ca_certs()
+    ctx.load_verify_locations(cafile=certifi.where())
+    cacerts = ctx.get_ca_certs()
+    assert len(cacerts)
