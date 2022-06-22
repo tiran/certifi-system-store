dc06c58bd3b4a38904e9ab8ef6c1be181ea9f2c8
Christian Heimes <christian@python.org>
2022-06-22 09:31:08 +0200

    Update CI, drop FreeBSD testing (#20)

--- .github/entry.sh
+++ .github/entry.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 set -ex
 
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-fi
-
-# check that default setuptools can handle setup.cfg and setup.py
-python3 -m pip list
-if [ "$VERSION_ID" != "18.04" ]; then
-    # setuptools on Ubuntu Bionic doesn't like attr: with src/
-    python3 setup.py egg_info sdist
-fi
-
-# update to latest setuptools, pipm and tox
-python3 -m pip install --upgrade pip setuptools tox
-python3 -m tox -e py3-test,py3-requests
+# run in venv to work around Debian/Ubuntu downstream patches
+# --upgrade-deps is not available in older distros
+python3 -m venv testenv
+testenv/bin/python3 -m pip install --upgrade pip setuptools
+testenv/bin/python3 -m pip install tox
+testenv/bin/python3 -m tox -e py3-test,py3-requests
